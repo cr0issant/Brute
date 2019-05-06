6918356d23bf2ed5c64498530292bfb6b380e161
cr0issant <matthieu.pradeau@aquilenet.fr>
2019-05-06 17:33:11 +0200

    Update SimpleBruteLetterFacto.py

--- SimpleBruteLetterFacto.py
+++ SimpleBruteLetterFacto.py
@@ -14,21 +14,19 @@ while mot != 'arrive': # Mot de passe en 6 lettres
 
     mot = ma_liste[mdpo [5]] + ma_liste[mdpo [4]] + ma_liste[mdpo [3]] + ma_liste[mdpo [2]] + ma_liste[mdpo [1]] + ma_liste[mdpo [0]]
 
-    if tempo == 1000000:
-        print(mot)
-        tempo = 0
-    tempo += 1
-
     mdpo [0] += 1 # On incrémente
     op += 1
     for i in range ( len(mdpo) ):
         if mdpo [i] == 26:
             mdpo [i] = 0
             mdpo [i+1] += 1
-        #else : 
-            #break
 
+    if tempo == 1000000:
+        print(mot)
+        tempo = 0
+    tempo += 1
 
+    
 print(mot)
 temps = time.time() - temps
 print( temps )
